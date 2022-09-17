7897b9d133a1d96d371f7e3c946d606cd9d6289b
Tifilin <charliearglist@gmail.com>
2022-09-18 02:43:00 +0300

    implement HW_1.1 adjustmens_1

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -1,37 +1,38 @@
 public class VariablesTheme {
 
     public static void main(String[] args) {
-
-        boolean license = true;
-        byte kolvoProcessorov = 1;
-        char edition = 'H';
-        short type = 64;
-        int memory = 6011;
-        long build = 14393;
-        float version = 10.0f;
-        double chastota = 2.203;
+        boolean licenseStatus = true;
+        byte cpuAmount = 1;
+        char osEdition = 'H';
+        short bitVersion = 64;
+        int ramSize = 6011;
+        long osBuild = 14393;
+        float osVersion = 10.0f;
+        double cpuFrequency = 2.203;
 
         System.out.println("\nЗадачи по теме \"Переменные: примитивные типы данных\"");
+
         System.out.println("\n1 Создание переменных и вывод их значений на консоль");
-        System.out.println("\n    Наличие лицензии: " + license);
-        System.out.println("    Количество процессоров: " + kolvoProcessorov);
-        System.out.println("    Частота, Гц: " + chastota);
-        System.out.println("    Полный объём физической памяти, мб: " + memory);
-        System.out.println("    Тип системы: " + type);
-        System.out.println("    Версия ОС: " + version);
-        System.out.println("    Вид ОС: " + edition);
-        System.out.println("    Построение: " + build);
+        System.out.println("\n    Наличие лицензии: " + licenseStatus);
+        System.out.println("    Количество процессоров: " + cpuAmount);
+        System.out.println("    Частота, Гц: " + cpuFrequency);
+        System.out.println("    Полный объём физической памяти, мб: " + ramSize);
+        System.out.println("    Тип системы: " + bitVersion);
+        System.out.println("    Версия ОС: " + osVersion);
+        System.out.println("    Вид ОС: " + osEdition);
+        System.out.println("    Построение: " + osBuild);
 
         System.out.println("\n2 Расчет стоимости товара со скидкой");
 
         short penPrice = 100;
         short bookPrice = 200;
-        short discountSize = 11;
-        float discountAmount = (penPrice + bookPrice) * discountSize / 100;
-        float resulPrice = penPrice + bookPrice - discountAmount;
+        short amountDiscount = 11;
+        float sumDiscount = (penPrice + bookPrice) * amountDiscount / 100;
+        float discountPrice = penPrice + bookPrice - sumDiscount;
 
-        System.out.println("    Сумма скидки: " + discountAmount);
-        System.out.println("    Общая стоимость товаров со скидкой: " + resulPrice);
+        System.out.println("    Сумма скидки: " + sumDiscount);
+        System.out.println("    Общая стоимость товаров со скидкой: " + discountPrice);
+
         System.out.println("\n3 Вывод на консоль слова JAVA");
         System.out.println("   J    a  v     v  a");
         System.out.println("   J   a a  v   v  a a");
@@ -40,65 +41,70 @@ public class VariablesTheme {
 
         System.out.println("\n4 Отображение min и max значений числовых типов данных");
 
-        byte varByte = 127;
-        short varShort = 32767;
-        int varInt = 2147483647;
-        long varLong = 9223372036854775807L;
+        byte byteMax = 127;
+        short shortMax = 32767;
+        int intMax = 2147483647;
+        long longMax = 9223372036854775807L;
 
-        System.out.println("    Первоначальное значение переменной типа byte: " + varByte);
-        System.out.println("    Значение после инкремента переменной типа byte: " + (++varByte));
-        System.out.println("    Значение после декремента переменной типа byte: " + (--varByte));
-        System.out.println("    Первоначальное значение переменной типа short: " + varShort);
-        System.out.println("    Значение после инкремента переменной типа short: " + (++varShort));
-        System.out.println("    Значение после декремента переменной типа short: " + (--varShort));
-        System.out.println("    Первоначальное значение переменной типа int: " + varInt);
-        System.out.println("    Значение после инкремента переменной типа int: " + (++varInt));
-        System.out.println("    Значение после декремента переменной типа int: " + (--varInt));
-        System.out.println("    Первоначальное значение переменной типа long: " + varLong);
-        System.out.println("    Значение после инкремента переменной типа long: " + (++varLong));
-        System.out.println("    Значение после декремента переменной типа long: " + (--varLong));
+        System.out.println("    Первоначальное значение переменной типа byte: " + byteMax);
+        System.out.println("    Значение после инкремента переменной типа byte: " + (++byteMax));
+        System.out.println("    Значение после декремента переменной типа byte: " + (--byteMax));
+        System.out.println("    Первоначальное значение переменной типа short: " + shortMax);
+        System.out.println("    Значение после инкремента переменной типа short: " + (++shortMax));
+        System.out.println("    Значение после декремента переменной типа short: " + (--shortMax));
+        System.out.println("    Первоначальное значение переменной типа int: " + intMax);
+        System.out.println("    Значение после инкремента переменной типа int: " + (++intMax));
+        System.out.println("    Значение после декремента переменной типа int: " + (--intMax));
+        System.out.println("    Первоначальное значение переменной типа long: " + longMax);
+        System.out.println("    Значение после инкремента переменной типа long: " + (++longMax));
+        System.out.println("    Значение после декремента переменной типа long: " + (--longMax));
 
         System.out.println("\n5 Перестановка значений переменных");
 
-        int first = 2;
-        int second = 5;
-        int third = first;
+        int num1 = 2;
+        int num2 = 5;
 
         System.out.println("5.1 Перестановка с использованием третьей переменной");
         System.out.println("Исходные значения переменных:");
-        System.out.println("Первая:" + first + ", Вторая:" + second + ", Третья:" + third);
+        System.out.println("Первая:" + num1 + ", Вторая:" + num2);
 
-        first = second;
-        second = third;
+        int tmp = num1;
+
+        num1 = num2;
+        num2 = tmp;
 
         System.out.println("Новые значения переменных:");
-        System.out.println("Первая:" + first + ", Вторая:" + second + ", Третья:" + third);
+        System.out.println("Первая:" + num1 + ", Вторая:" + num2);
+
         System.out.println("\n5.2 Перестановка с помощью арифметических операций");
         System.out.println("Исходные значения переменных:");
-        System.out.println("Первая:" + first + ", Вторая:" + second);
+        System.out.println("Первая:" + num1 + ", Вторая:" + num2);
 
-        first = first - 3;
-        second = first + 3;
+        num1 = num1 - num2;
+        num2 = num2 + num1;
+        num1 = num2 - num1;
 
         System.out.println("Новые значения переменных:");
-        System.out.println("Первая:" + first + ", Вторая:" + second);
+        System.out.println("Первая:" + num1 + ", Вторая:" + num2);
+
         System.out.println("\n5.3 Перестановка с помощью побитовой операции");
         System.out.println("Исходные значения переменных:");
-        System.out.println("Первая:" + first + ", Вторая:" + second);
+        System.out.println("Первая:" + num1 + ", Вторая:" + num2);
 
-        second = second ^ first ^ second;
-        first = first ^ 5 ^ first;
+        num1 = num2 ^ num1;
+        num2 = num1 ^ num2;
+        num1 = num2 ^ num1;
 
         System.out.println("Новые значения переменных:");
-        System.out.println("Первая:" + first + ", Вторая:" + second);
+        System.out.println("Первая:" + num1 + ", Вторая:" + num2);
 
         System.out.println("\n6 Вывод символов и их кодов");
 
-        char firstChar = 35;
-        char secondChar = 38;
-        char thirdChar = 64;
-        char fourthChar = 94;
-        char fifthChar = 95;
+        char firstChar = '#';
+        char secondChar = '&';
+        char thirdChar = '@';
+        char fourthChar = '^';
+        char fifthChar = '_';
 
         System.out.println((short) firstChar + " " + firstChar);
         System.out.println((short) secondChar + " " + secondChar);
@@ -117,18 +123,19 @@ public class VariablesTheme {
               
         System.out.println("\n8 Вывод на консоль ASCII-арт Дюка");
 
-        char one = '/';
-        char two = '\\';
-        char thr = '_';
-        char fou = '(';
-        char fiv = ')';
-        char six = ' ';
+        char forwardslash = '/';
+        char doubleBackslash = '\\';
+        char underscore = '_';
+        char openParenthesis = '(';
+        char closeParenthesis = ')';
+        char space = ' ';
 
-        System.out.println(six + "" + six + "" + six + "" + six + "" + one + "" + two + "" + six + "" + six + "" + six + "" + six);
-        System.out.println(six + "" + six + "" + six + "" + one + "" + six + "" + six + "" + two + "" + six + "" + six + "" + six);
-        System.out.println(six + "" + six + "" + one + "" + thr + "" + fou + "" + six + "" + fiv + "" + two + "" + six + "" + six);
-        System.out.println(six + "" + one + "" + six + "" + six + "" + six + "" + six + "" + six + "" + six + "" + two + "" + six);
-        System.out.println(one + "" + thr + "" + thr + "" + thr + "" + thr + "" + one + "" + two + "" + thr + "" + thr + "" + two);
+        System.out.println("" + space + space + space + space + forwardslash + doubleBackslash + space + space + space + space);
+        System.out.println("" + space + space + space + forwardslash + space + space + doubleBackslash + space + space + space);
+        System.out.println("" + space + space + forwardslash + underscore + openParenthesis + space + closeParenthesis + doubleBackslash + space + space);
+        System.out.println("" + space + forwardslash + space + space + space + space + space + space + doubleBackslash + space);
+        System.out.println("" + forwardslash + underscore + underscore + underscore + underscore + forwardslash + doubleBackslash + underscore + underscore + doubleBackslash);
+
         System.out.println("\n9 Произведение и сумма цифр числа");
 
         number = 345;
@@ -138,15 +145,16 @@ public class VariablesTheme {
 
         System.out.println("i.  Сумма цифр числа " + number + " = " + (hundreds + dozens + units));
         System.out.println("ii. Произведение цифр числа " + number + " = " + hundreds * dozens * units);
+
         System.out.println("\n10 Преобразование секунд");
 
-        int amount = 86399;
-        short seconds = (short) (amount % 60);
-        short minutes = (short) (amount / 60);
-        short hours = (short) (minutes / 60);
+        int secondsAmount = 86399;
+        int resultSeconds = secondsAmount % 60;
+        int resultMinutes = secondsAmount / 60;
+        int resultHours = resultMinutes / 60;
 
-        minutes = (short) (minutes % 60);
+        resultMinutes = resultMinutes % 60;
 
-        System.out.println("Получилось (ЧЧ:ММ:СС): " + hours + ":" + minutes + ":" + seconds);
+        System.out.println("Получилось (ЧЧ:ММ:СС): " + resultHours + ":" + resultMinutes + ":" + resultSeconds);
     }
 }
